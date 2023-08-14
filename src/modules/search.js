e8d82a7315da03d675511d5b8df3f650f04fc5dc
yanaivantsova <yanaivantsova@yandex.by>
2023-08-14 22:28:27 +0200

    feat: add debounce and price, sale filtration

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,17 +1,45 @@
 import getData from "./getData";
 import renderGoods from "./renderGoods";
-import { searchFilter } from "./filters";
+import { funcFilter} from "./filters";
+import { debounce } from "./helpers";
 
 
-const search = ()=> {
+const search = () => {
     const searchInput = document.querySelector('.search-wrapper_input')
-searchInput.addEventListener('input', (event)=>{
-const value = event.target.value;
+    const minInp = document.getElementById('min')
+    const maxInp = document.getElementById('max')
+    const checkInp = document.getElementById('discount-checkbox')
+    const checkMark = document.querySelector('.filter-check_checkmark')
 
-getData().then((data) => {
-    renderGoods(searchFilter(data, value));
-})
-})
+
+
+    const debounceFunc = debounce((min = '', max = '', checkValue = false, searchValue = '') => {
+        getData().then((data => {
+            renderGoods(funcFilter(data, min, max, checkValue, searchValue))
+        }))
+    })
+
+
+    searchInput.addEventListener('input', ()=>{
+        debounceFunc(minInp.value, maxInp.value, checkInp.checked, searchInput.value);
+    })
+
+    minInp.addEventListener('input', () => {
+        debounceFunc(minInp.value, maxInp.value, checkInp.checked, searchInput.value);
+    })
+
+    maxInp.addEventListener('input', () => {
+        debounceFunc(minInp.value, maxInp.value, checkInp.checked, searchInput.value);
+    })
+
+    checkInp.addEventListener('input', (event) => {
+        if (checkInp.checked) {
+            checkMark.classList.add('checked')
+        } else {
+            checkMark.classList.remove('checked')
+        }
+        debounceFunc(minInp.value, maxInp.value, checkInp.checked, searchInput.value);
+    })
 }
 
 export default search
